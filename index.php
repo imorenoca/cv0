809d88bc8026c76d3fd227713368ccd6c63cd27d
imorenoca <153509984+imorenoca@users.noreply.github.com>
2026-09-22 13:01:16 +0200

    feature: Se configura .htaccess, único punto de entrada, se traslada pantalla de inicio a bienvenida,  e inicio.php se conserva como enrutador

--- index.php
+++ index.php
@@ -1,50 +1,12 @@
+<?php
+$rutas = [
+    '/' => __DIR__ .'/vistas/bienvenida.php',
+];
 
-<?php require_once "vistas/modulos/header.php";
-require_once "vistas/modulos/menu.php";
+$ruta_silicitada = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-?>
-
-
-<body>
-
-
-    <div class="container mt-5">
-        <!-- Primer apartado -->
-        <div class="row">
-            <div class="col-md-8 mx-auto text-center">
-                <h1>Bienvenid@ a cvWeb</h1>
-            </div>
-        </div>
-
-        <!-- Segundo apartado -->
-        <div class="row mt-4">
-            <div class="col-md-8 mx-auto text-center">
-                <h3>Encuentra el trabajo perfecto para ti</h3>
-            </div>
-        </div>
-
-        <!-- Tercer apartado -->
-        <div class="row mt-4">
-            <div class="col-md-8 mx-auto text-center">
-                <p>cvWeb es la plataforma que te ayuda a gestionar las ofertas de empleo para que puedas encontrar el trabajo perfecto para ti. </p>
-                <p>Podrás realizar el seguimiento de tus ofertas de trabajo enviadas en un sólo lugar.</p>
-            </div>
-        </div>
-        <!-- Cuarto apartado -->
-        <div class="row mt-4">
-            <div class="col-md-6 offset-md-3 text-center">
-                <!-- Botón para ir a la página de login -->
-                <a href="vistas/login.php" class="btn btn-success btn-lg btn-block">Login</a>
-                <!-- Botón para ir a la página de registro -->
-                <a href="vistas/register.php" class="btn btn-primary btn-lg btn-block">Registro</a>
-            </div>
-        </div>
-    </div>
-
-
-    <br>
-    <br><br><br><br><br><br>
-    <?php require_once "vistas/modulos/footer.php"; ?>
-</body>
-
-</html>
+if (array_key_exists($ruta_silicitada, $rutas)) {
+    require_once $rutas[$ruta_silicitada];
+} else {
+    require_once __DIR__ . '/vistas/error404.php';
+}
---
+++ vistas/error404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página no encontrada</title>
+</head>
+<body>
+    <h1>404</h1>
+    <p>La página que buscas no existe.</p>
+    <a href="/">Volver al inicio</a>
+</body>
+</html>
