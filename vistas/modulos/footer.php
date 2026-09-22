fde0ea816f2e857eeb569550d60ad07c2aa644ae
imorenoca <153509984+imorenoca@users.noreply.github.com>
2026-09-22 13:22:07 +0200

    feature: Menor. Se actualiza versión en footer

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
        <div class="pull-right hidden-xs text-center">
-           <b>Version 1.0 </b>
+           <b>Version 2.0 </b>
        
-       <strong>Copyright &copy; 2023 isamor.</strong> All rights reserved.
+       <strong>Copyright &copy; 2026 isamor.</strong> All rights reserved.
        </div>
    </footer>
